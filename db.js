451d192a9be68844506951ec2cdf1c87f06cf173
Kalman Tarnay <kalmisoft@gmail.com>
2012-05-23 20:40:13 +0200

    db into separate file

---
+++ db.js
@@ -0,0 +1,39 @@
+var wiki_base = "https://wiki.sch.bme.hu/bin/view/Infoalap/";
+var db = {
+
+  "anal2" : {
+    "nickname" : "anal2",
+    "wiki_url" : wiki_base + "Anal2",
+  },
+  
+  "fizika2" : {
+    "nickname" : "fizika2",
+    "wiki_url" : wiki_base + "Fizika2",
+  },
+  
+  "algel" : {
+    "nickname" : "algel",
+    "wiki_url" : wiki_base + "AlgEl",
+  },
+  
+  "opre" : {
+    "nickname" : "opre",
+    "wiki_url" : wiki_base + "OpRe",
+  },
+  
+  "halok" : {
+    "nickname" : "halok",
+    "wiki_url" : wiki_base + "SzgHalok",
+  },
+
+  "bsz2" : {
+    "nickname" : "bsz2",
+    "wiki_url" : wiki_base + "BsZ2",
+  },
+
+  "sznikak" : {
+    "nickname" : "sznikak",
+    "wiki_url" : wiki_base + "SzoftverTechnikak",
+  },
+  
+}
